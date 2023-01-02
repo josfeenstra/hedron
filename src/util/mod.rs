0b5b36f2fe16d01a9acbb4d0d858a5aa0ebeb5b9
Jos Feenstra <jos@geodelta.com>
2023-01-02 15:25:00 +0100

    add a transpose function

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -2,10 +2,12 @@ mod dir;
 mod iter;
 mod named_enum;
 mod rot;
+mod set;
 mod simple_step_range;
 
 pub use dir::*;
 pub use iter::*;
 pub use named_enum::*;
 pub use rot::*;
+pub use set::*;
 pub use simple_step_range::*;
--- src/util/set.rs
+++ src/util/set.rs
@@ -1,1 +1,13 @@
-
+pub fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    assert!(!v.is_empty());
+    let len = v[0].len();
+    let mut iters: Vec<_> = v.into_iter().map(|n| n.into_iter()).collect();
+    (0..len)
+        .map(|_| {
+            iters
+                .iter_mut()
+                .map(|n| n.next().unwrap())
+                .collect::<Vec<T>>()
+        })
+        .collect()
+}
